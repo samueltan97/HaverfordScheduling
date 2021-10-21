499388faa5dd66b139c68356b882a6676df0bac6
dylansoemitro <44148714+dylansoemitro@users.noreply.github.com>
2021-10-20 20:09:30 -0400

    main

---
+++ Algorithm/main.py
@@ -0,0 +1,32 @@
+
+class Class:
+    def __init__(self, id):
+        self.id = id
+
+
+def sortClasses(S,C):
+    classInterestCount = {}
+    for s in S:
+        for p in S[s]:
+            #and p is not a first year seminar
+            if p in classInterestCount.keys():
+                classInterestCount[p] = 1
+            else:
+                #if p first year seminar, classInterestCount[p] = 0
+                classInterestCount[p] = 1
+    #sort by highest interest, descending order
+    sorted_classInterestCount = sorted(classInterestCount.items(), key=lambda x: x[1], reverse=True)
+    return sorted_classInterestCount
+
+def sortClassTimes(T):
+    classTimeConflicts = {}
+    sortedClassTimes = []
+
+
+
+def identifyRoomsForClass(R,C):
+    sorted_Rooms = sorted(R.items(), key=lambda x: x[1], reverse=True)
+    return sorted_Rooms
+
+def setUpAvailabilty(input, sortedClassTimes):
+
