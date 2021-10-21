9a057d97e20b12a2323187a1899aefafc1cccafe
Samuel <samueltan97@hotmail.com>
2021-10-21 09:50:58 -0400

    Adding eval script

--- misc/test.py
+++ misc/test.py
@@ -20,6 +20,45 @@ def parse_args(description):
             sys.exit()
     return opts
 
+def read_preferences(pref_filename):
+    p_file = open(pref_filename, "r")
+    p_data = p_file.readlines()
+    p_file.close()
+    student_dict = dict()
+    num_students = p_data[0].strip().split()[1]
+    p_data = p_data[1:]
+    for row in p_data:
+        row_data = row.strip().split()
+        student_dict[row_data[0]] = row_data[1:]
+    return num_students, student_dict
+
+def read_constraints(constraint_filename):
+    c_file = open(constraint_filename, "r")
+    c_data = c_file.readlines()
+    c_file.close()
+    class_dict = dict()
+    num_class_times = c_data[0].strip().split()[2]
+    num_rooms = c_data[1].strip().split()[1]
+    end_room_index = 2+int(num_rooms)
+    room_dict = dict()
+    for row in c_data[2:end_room_index]:
+        row_data = row.strip().split()
+        room_dict[row_data[0]] = row_data[1]
+    num_classes = c_data[end_room_index].strip().split()[1]
+    num_teachers = c_data[end_room_index + 1].strip().split()[1]
+    teacher_dict = dict()
+    class_dict = dict()
+    for row in c_data[end_room_index + 2:]:
+        row_data = row.strip().split()
+        if class_dict.get(row_data[0]) is None:
+            class_dict[row_data[0]] = row_data[1]
+        if teacher_dict.get(row_data[1]) is None:
+            teacher_dict[row_data[1]] = {'class':[row_data[0]]}
+        else:
+            teacher_dict[row_data[1]]['class'].append(row_data[0])
+    return num_rooms, num_classes, num_class_times, num_teachers, room_dict, class_dict, teacher_dict
+
+
 def test(schedule_filename, constraint_filename, pref_filename):
     num_students, student_dict = read_preferences(pref_filename)
     num_rooms, num_classes, num_class_times, num_teachers, room_dict, class_dict, teacher_dict = read_constraints(constraint_filename)
@@ -110,6 +149,8 @@ def test(schedule_filename, constraint_filename, pref_filename):
                             sys.exit()
                     
                         student_preferences += 1
-                        
+
     print("Schedule is valid")
     print("Student preferences value:", student_preferences)
+
+def evaluate_runtime()
